339de30fb4261ceb47b66005acae66e6106b949c
Burning_Wipf <Rafal.kuka94@gmail.com>
2020-01-03 16:18:37 +0100

    Add files via upload

---
+++ calcDigest.py
@@ -0,0 +1,14 @@
+def calcDigest(pw,params):# dictionary keywords will be used to sort values. Only vals will be  concatinated then hashed with merchant password
+
+    import hashlib, hmac, urllib
+    to_sign = ""
+    params['result'] = 0
+    for v in sorted(params.keys()):
+        to_sign += str(params[v])
+    api_key = ''.join(pw).encode("utf8")
+    print('calculated from: '+to_sign)
+    to_sign = to_sign.encode("utf8")
+    digest = hmac.new(api_key, to_sign, digestmod=hashlib.sha256).hexdigest()
+
+    return digest
+
